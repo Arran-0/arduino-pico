8c046aac0e4005f1f3aef96af3379af2cdd78f6d
Arran <83124956+Arran-0@users.noreply.github.com>
2025-05-15 03:41:00 +0100

    Changed MAX_ATT_DB_SIZE from 512 to 4098.

--- include/rp2350/btstack_config.h
+++ include/rp2350/btstack_config.h
@@ -58,7 +58,7 @@
 #define NVM_NUM_LINK_KEYS 16
 
 // We don't give btstack a malloc, so use a fixed-size ATT DB.
-#define MAX_ATT_DB_SIZE 512
+#define MAX_ATT_DB_SIZE 4098
 
 // BTstack HAL configuration
 #define HAVE_EMBEDDED_TIME_MS
